819399f23827ff4d43a49f1974db4af439f85d58
sumancrest0001 <suman.crest0001@gmail.com>
2020-06-20 15:41:09 +0545

    customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
